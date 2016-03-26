4e271897f98e04ea672981f86da3d2d42a6783ca
smallclover <932054296@qq.com>
2016-03-26 18:22:30 +0800

    双端链表
    增加注释说明

--- src/cn/jtools/algorithm/DoubleEndLinkedList.java
+++ src/cn/jtools/algorithm/DoubleEndLinkedList.java
@@ -8,37 +8,61 @@ package cn.jtools.algorithm;
  */
 public class DoubleEndLinkedList {
 
+	/**
+	 * 
+	 * 链表存储的节点类型
+	 * 
+	 */
 	private class Data {
-		private Object obj;
-		private Data next = null;
+		private Object obj;// 存储数据
+		private Data next = null;// 指向下一个链表节点
 
 		Data(Object obj) {
 			this.obj = obj;
 		}
 	}
 
-	private Data first = null;
-	private Data last = null;
+	private Data first = null;// 头结点
+	private Data last = null;// 尾结点
 
+	/**
+	 * 从头结点插入
+	 * 
+	 * @param obj
+	 */
 	public void insertFirst(Object obj) {
 		Data data = new Data(obj);
+		// 如果头结点为空，说明链表为空,头结点和尾节点都指向同一个结点。
 		if (first == null) {
 			last = data;
 		}
-		data.next = first;
-		first = data;
+		data.next = first;// 原先的first结点设置为新添加的data的next结点
+		first = data;// 将新添加的data设置为first结点
 	}
 
+	/**
+	 * 从尾节点插入
+	 * 
+	 * @param obj
+	 */
 	public void insertLast(Object obj) {
 		Data data = new Data(obj);
 		if (first == null) {
 			first = data;
 		} else {
-			last.next = data;
+			last.next = data;// 将数据添加到尾部
+			// System.out.println(last.next);
 		}
-		last = data;
+		last = data;// 将last指向新添加的数据
+		// System.out.println(last);
 	}
 
+	/**
+	 * 删除头结点
+	 * 
+	 * @return 返回该头结点的数值
+	 * @throws Exception
+	 */
 	public Object deleteFirst() throws Exception {
 		if (first == null) {
 			throw new Exception("empty");
@@ -51,6 +75,11 @@ public class DoubleEndLinkedList {
 		return temp.obj;
 	}
 
+	/**
+	 * 删除尾结点
+	 * 
+	 * @throws Exception
+	 */
 	public void deleteLast() throws Exception {
 		if (first == null) {
 			throw new Exception("empty");
@@ -72,6 +101,13 @@ public class DoubleEndLinkedList {
 
 	}
 
+	/**
+	 * 查找指定的结点
+	 * 
+	 * @param obj
+	 * @return 指定的对象结点
+	 * @throws Exception
+	 */
 	public Object find(Object obj) throws Exception {
 		if (first == null) {
 			throw new Exception("empty");
@@ -91,6 +127,10 @@ public class DoubleEndLinkedList {
 	/*
 	 * public boolean isEmpty() { return (first == null); }
 	 */
+
+	/**
+	 * 打印所有结点
+	 */
 	public void dispaly() {
 		if (first == null) {
 			System.out.println("empty");
@@ -102,5 +142,13 @@ public class DoubleEndLinkedList {
 		}
 
 		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		DoubleEndLinkedList dell = new DoubleEndLinkedList();
+		dell.insertFirst(2);
+		dell.insertFirst(1);
+		dell.insertLast(3);
+		dell.dispaly();
 	}
 }
