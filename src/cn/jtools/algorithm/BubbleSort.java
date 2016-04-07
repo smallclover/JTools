e289f603aec6161766fe03046841bd184433b002
smallclover <18363998103@163.com>
2016-04-07 21:57:53 +0800

    BubbleSort

--- src/cn/jtools/algorithm/BubbleSort.java
+++ src/cn/jtools/algorithm/BubbleSort.java
@@ -24,6 +24,24 @@ public class BubbleSort {
 			System.out.println();
 		}	
 	}
+	//Test第一个数不用交换？？
+	public static void buddleSort2(int[] arr){
+		for(int i = 1; i < arr.length - 1; i++){
+			for(int j = 1; j < arr.length - i; j++){
+				int temp;
+				if(arr[j + 1] < arr[j]){
+					temp = arr[j ];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+			for (int k : arr) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}	
+	}
+	
 	
 	//通过设置标志符加以优化
 	public static void buddleSortAdvanced(int[] arr){
@@ -50,7 +68,8 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		int [] arr = {4,3,6,7,8,1,5,9,2,10};
 		int [] arr1 = {1,2,3,4,5,6,7,8,9,10};
-		buddleSortAdvanced(arr1);
+//		buddleSortAdvanced(arr1);
+		buddleSort2(arr1);
 //		buddleSort(arr);
 //		for (int i : arr) {
 //			System.out.print(i + " ");
