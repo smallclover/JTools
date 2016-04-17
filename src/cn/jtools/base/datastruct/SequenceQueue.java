796a9a6dd39b3d136f8f414eb6e2b6923995eff3
smallclover <18363998103@163.com>
2016-04-17 22:15:36 +0800

    顺序队列

--- src/cn/jtools/base/datastruct/SequenceQueue.java
+++ src/cn/jtools/base/datastruct/SequenceQueue.java
@@ -1,26 +1,39 @@
 package cn.jtools.base.datastruct;
 
-import javax.sound.midi.Sequence;
-
+/**
+ * 顺序队列
+ * @author smallclover
+ *
+ * @param <E>
+ */
 public class SequenceQueue<E>{
 
-	E[] a;
-	private static final int DEFAULT_SIZE = 10;
+	E[] a;//对象数组， 队列最多存储a.length - 1个对象
+	private static final int DEFAULT_SIZE = 10;//默认初始化大小
 	
-	int head;
+	int head;//队首下标
 	
-	int tail;
+	int tail;//队尾下标
 	
 	public SequenceQueue(){
 		this(DEFAULT_SIZE);
 	}
 	
+	/**
+	 * 初始化指定长度的队列
+	 * @param size
+	 */
 	public SequenceQueue(int size){
 		a = (E[]) new Object[size];
 		head = 0;
 		tail = 0;
 	}
 	
+	/**
+	 * 队尾入队
+	 * @param obj
+	 * @return 队列满的时候返回false，否则返回true
+	 */
 	public boolean enQuene(E obj){
 		if((tail + 1) % a.length == head){
 			return false;
@@ -31,6 +44,10 @@ public class SequenceQueue<E>{
 		}
 	}
 	
+	/**
+	 * 队首出队
+	 * @return
+	 */
 	public E deQueue(){
 		if(tail == head){
 			return null;
@@ -41,10 +58,15 @@ public class SequenceQueue<E>{
 		}
 	}
 	
+	/**
+	 * 队列长度
+	 * @return
+	 */
 	public int size(){
 		return (tail - head)&(a.length - 1);
 	}
 	
+	//?
 	public int length(){
 		if(tail > head){
 			return tail - head;
@@ -53,6 +75,10 @@ public class SequenceQueue<E>{
 		}
 	}
 	
+	/**
+	 * 判断是否为空
+	 * @return
+	 */
 	public boolean isEmpty(){
 		return tail == head;
 	}
