0ff2eb1dac0aa97f44fdc03768b9f85c7e73cdad
smallclover <18363998103@163.com>
2016-04-25 22:43:34 +0800

    日志测试

--- src/cn/jtools/test/Test_log.java
+++ src/cn/jtools/test/Test_log.java
@@ -2,12 +2,16 @@ package cn.jtools.test;
 
 import cn.jtools.log.ILogger;
 import cn.jtools.log.ILogger.ILevel;
+import cn.jtools.log.LogFactory;
 import cn.jtools.log.impl.JDKLoggerFactory;
 
 public class Test_log {
 	public static ILogger iLogger = new JDKLoggerFactory().getLogger("Test_log");
 	
+	public static ILogger iLogger2 = LogFactory.getLogger(Test_log.class);
 	public static void main(String[] args) {
 		iLogger.log(ILevel.WARN, "Test_log");
+		iLogger2.log(ILevel.WARN, "Test_log");
+		iLogger2.warn("test_log2");
 	}
 }
